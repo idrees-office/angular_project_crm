ac1be4d0f5607798b2c59a8ebaf809e5aa7a40f1
idrees-office <idreesoffice9012@gmail.com>
2024-02-27 23:36:04 -0800

    update route file

--- src/app/pages/dashboards/dashboards.routing.ts
+++ src/app/pages/dashboards/dashboards.routing.ts
@@ -4,11 +4,15 @@ import { AppDashboard1Component } from './dashboard1/dashboard1.component';
 import { AppDashboard2Component } from './dashboard2/dashboard2.component';
 
 
-
 export const DashboardsRoutes: Routes = [
   {
-    path: '',
+    path: 'dashboards', // Add a parent path for all dashboard routes
     children: [
+      {
+        path: '', // Empty path for default dashboard
+        redirectTo: 'dashboard1',
+        pathMatch: 'full',
+      },
       {
         path: 'dashboard1',
         component: AppDashboard1Component,
@@ -45,6 +49,48 @@ export const DashboardsRoutes: Routes = [
     ],
   },
 ];
+
+
+// export const DashboardsRoutes: Routes = [
+//   {
+//     path: '',
+//     children: [
+//       {
+//         path: 'dashboard1',
+//         component: AppDashboard1Component,
+//         data: {
+//           title: 'Analytical',
+//           urls: [
+//             { title: 'Dashboard', url: '/dashboards/dashboard1' },
+//             { title: 'Analytical' },
+//           ],
+//         },
+//       },
+//       {
+//         path: 'dashboard2',
+//         component: AppDashboard2Component,
+//         data: {
+//           title: 'eCommerce',
+//           urls: [
+//             { title: 'Dashboard', url: '/dashboards/dashboard1' },
+//             { title: 'eCommerce' },
+//           ],
+//         },
+//       },
+//       {
+//         path: 'dashboard1/:type',
+//         component: AppDashboard1Component,
+//         data: {
+//           title: 'Email',
+//           urls: [
+//             { title: 'Dashboard', url: '/dashboards/dashboard1' },
+//             { title: 'Email' },
+//           ],
+//         },
+//       },
+//     ],
+//   },
+// ];
 
 
 // export const DashboardsRoutes: Routes = [
