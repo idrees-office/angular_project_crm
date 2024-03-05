fceccd6ddc955329d802d24bb6c671d6aa3bc3a4
Marsad Akbar <36798110+Marsad1998@users.noreply.github.com>
2024-03-06 00:33:53 +0500

    Update environments.dev.ts

--- src/environments/environments.dev.ts
+++ src/environments/environments.dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    baseUrl: 'http://127.0.0.1:8000/api'  
-    // baseUrl: 'http://10.99.1.77:8000/api'  
-      // baseUrl : 'https://newcrmbackend.evernestre.ae/api'   
+    //baseUrl: 'http://127.0.0.1:8000/api'  
+    //baseUrl: 'http://10.99.1.77:8000/api'  
+    baseUrl : 'https://newcrmbackend.evernestre.ae/api'   
   };
   
