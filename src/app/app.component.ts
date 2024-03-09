8da5ddf9c00d129e5441c5eb3f03c05e769cd0b1
idrees-office <idreesoffice9012@gmail.com>
2024-03-09 04:38:13 -0800

    fb code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare const FB: any;
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Evernest Real Estate';
+
+  ngOnInit(): void {
+    this.initFacebookSdk();
+  }
+
+  initFacebookSdk(): void {
+    FB.init({
+      appId: '1067623880812441',
+      autoLogAppEvents: true,
+      xfbml: true,
+      version: 'v12.0',
+    });
+  }
 }
