a6b887c18c00b813bbe065fe85ce1b06f95810b7
idrees-office <idreesoffice9012@gmail.com>
2024-03-09 05:30:25 -0800

    fb code abcdd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,21 +13,23 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.initFacebookSdk();
 
-
     var pageAccessToken =
       'EAAPKZC44rZA5kBO06ZBZAF8bCyGQQb1cdez6oj89BUWZAZCFP5F4CM4ZAmHm10xW7bbJT2LxLDkvDfE336oK5qoNZAb5ZA4lZAHJTpwADZCrxNQGYWnvQn45CJjO9as2UkSTJWuUUpCIaZBvxZCXe3EQnCWY5ZAsZA9hSIHgNnQGiz1jz7vbFH62R4xBsHbDK57Wt6ZAEzs2tgjIQd0iULyUAdfkbMnMUvcUk27RPMoZAMwZDZD|faketoken';
-    FB.api('/me/conversations', {
-      access_token: pageAccessToken,
-    }, function(response:any){
-      console.log(response);
-    });
-
-
+    FB.api(
+      '/me/conversations',
+      {
+        access_token: pageAccessToken,
+      },
+      function (response: any) {
+        console.log(response);
+      }
+    );
   }
+  // 111856167337521
 
   initFacebookSdk(): void {
     FB.init({
-      appId: '111856167337521',
+      appId: '1067623880812441',
       xfbml: true,
       version: 'v19.0',
     });
